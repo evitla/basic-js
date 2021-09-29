00334dceaec4c0e84823dcecb58aed076b21f64a
evitla <s.dinmuhamed@gmail.com>
2021-09-29 16:20:21 +0600

    feat: solve carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,5 +1,3 @@
-import { NotImplementedError } from '../extensions/index.js';
-
 const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
 
@@ -17,7 +15,13 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-export default function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function dateSample(sampleActivity) {
+  const isNotValid = typeof sampleActivity != "string" ||
+                     isNaN(sampleActivity) ||
+                     sampleActivity > MODERN_ACTIVITY ||
+                     sampleActivity < 1;
+
+  if (isNotValid) return false;
+
+  return Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / 0.693 * HALF_LIFE_PERIOD);
 }
